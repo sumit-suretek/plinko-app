1288abf86d89829de6eb30d47a924810bb9a058f
Sumit Sharma <33143914+sumit-suretek@users.noreply.github.com>
2018-04-05 12:43:58 +0530

    Update qlinqo2.js

--- js/qlinqo2.js
+++ js/qlinqo2.js
@@ -459,16 +459,18 @@ Qlinqo.update = function() {
 			
 				Qlinqo.gameOverLayer.addChild(new Qlinqo.GameOverScreen(pointMsg));
 			}
-          
+      form = $('#frmplinko');
+      document.getElementById("sessdata").value = AESencryption(Qlinqo.currentPlayerPoints);    
 			if(Qlinqo.currentPlayerPoints == "0"){
 				$('#zeronumber').removeClass('hidden');
-				setTimeout(function(){ $('#zeronumber').addClass('hidden');}, 4000);
+        $.post(form.attr('action'),form.serialize());
+        $('#frmplinko')[0].reset();
+        setTimeout(function(){ location.reload();}, 5000);
 			}else{
-          			document.getElementById("sessdata").value = AESencryption(Qlinqo.currentPlayerPoints);
-          			form = $('#frmplinko');
+          			
           			if($('#email').val() !== '' && $('#fullname').val() !== ''){
                   			$.post(form.attr('action'),form.serialize());
-       					$('#finalmsg').show();
+       					        $('#finalmsg').show();
                   			$('#finalmsg').removeClass('hidden');
 	                		$('#frmplinko')[0].reset();
                   		setTimeout(function(){ location.reload();}, 7000);
@@ -486,10 +488,10 @@ Qlinqo.update = function() {
 		}
 	}
 
-	for(var i in Qlinqo.foregroundLayer.children)
-	{
-		Qlinqo.foregroundLayer.children[i].update();
-	}
+// 	for(var i in Qlinqo.foregroundLayer.children)
+// 	{
+// 		Qlinqo.foregroundLayer.children[i].update();
+// 	}
 
 	Qlinqo.world.Step(
 	   1 / 60   //frame-rate
