8f3ecd4efa7339ae68ce52e4627c690185816e7e
Sumit Sharma <33143914+sumit-suretek@users.noreply.github.com>
2018-04-11 16:00:09 +0530

    Update fireworks.js

--- js/fireworks.js
+++ js/fireworks.js
@@ -90,13 +90,13 @@ var FW = {
 	
 	Update: function() {
 		      console.log(FW.foregroundLayer);
-        console.log('Outside Loop:'+FW);
+        //console.log('Outside Loop:'+FW);
             //console.log('Inside loop:'+FW);	
 		//	error start
-// 		for(var i in FW.foregroundLayer.children)
-// 		{
-// 			FW.foregroundLayer.children[i].update();
-// 		}
+		for(var i in FW.foregroundLayer.children)
+		{
+ 			FW.foregroundLayer.children[i];
+		}
 		//	uncomment 	
 		FW.foregroundLayer.needsDisplay = true;//set dirty flag
 	},
