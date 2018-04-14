d80f2ae9261ac89e8652932125fc07ae0cadd2d4
Sumit Sharma <33143914+sumit-suretek@users.noreply.github.com>
2018-04-14 10:56:17 +0530

    Update qlinqo2.js

--- js/qlinqo2.js
+++ js/qlinqo2.js
@@ -1,6 +1,3 @@
-(function(){
-
-
 //http://paulirish.com/2011/requestanimationframe-for-smart-animating/
 // shim layer with setTimeout fallback
 window.requestAnimFrame = (function(){
@@ -1045,9 +1042,6 @@ Qlinqo.Util.parseShadow = function(shadows, em) {
 		}
 		return ret;
 	};
-
-
-})();
 	
 	
 //old qlinqo2.js file
