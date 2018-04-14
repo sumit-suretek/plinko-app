fd9aaaddebcfca0096f4d7d852cf57fd3ccb6004
Sumit Sharma <33143914+sumit-suretek@users.noreply.github.com>
2018-04-14 14:35:44 +0530

    Update qlinqo2.js

--- js/qlinqo2.js
+++ js/qlinqo2.js
@@ -45,7 +45,7 @@ var Qlinqo = {'foregroundLayer':null
 			, 'ballsLeft': 5
 			, 'ballsScored':0};
 
-exports.QlinqoSetup = Qlinqo.Setup = function (playfieldLayer, foregroundLayer, backgroundLayer, debugLayer, statusLayer, gameOverLayer) {
+Qlinqo.Setup = function (playfieldLayer, foregroundLayer, backgroundLayer, debugLayer, statusLayer, gameOverLayer) {
 
 	//setup sound effects
 	
@@ -1044,6 +1044,7 @@ Qlinqo.Util.parseShadow = function(shadows, em) {
 		}
 		return ret;
 	};
+	exports.Qlinqo = new Qlinqo;
 
 })(window);
 	
